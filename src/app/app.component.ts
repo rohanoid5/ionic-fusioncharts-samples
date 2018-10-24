0e2d663123b9507d9492bdd037f3bd99efc6b975
Rohan Dey <rohandey@LPMB-KOL-Rohan.local>
2018-10-24 13:29:37 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  dataSource: Object;
+  title: string;
+  items: any[] = [
+    { id: 1, value: 'Simple Column 2D' },
+    { id: 2, value: '3D Pie Chart' },
+    { id: 3, value: 'Update Chart Data' },
+    { id: 4, value: 'Listen to events from chart' },
+    { id: 5, value: 'Drill down' },
+    { id: 6, value: 'Gauge' },
+    { id: 7, value: 'Change chart type at runtime' },
+    { id: 8, value: 'Multiple Themes' }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+    this.title = 'Angular  FusionCharts Sample';
+
+    this.dataSource = {
+      chart: {
+        caption: 'Countries With Most Oil Reserves [2017-18]',
+        subCaption: 'In MMbbl = One Million barrels',
+        xAxisName: 'Country',
+        yAxisName: 'Reserves (MMbbl)',
+        numberSuffix: 'K',
+        theme: 'fusion'
+      },
+      data: [
+        { label: 'Venezuela', value: '290' },
+        { label: 'Saudi', value: '260' },
+        { label: 'Canada', value: '180' },
+        { label: 'Iran', value: '140' },
+        { label: 'Russia', value: '115' },
+        { label: 'UAE', value: '100' },
+        { label: 'US', value: '30' },
+        { label: 'China', value: '30' }
+      ]
+    };
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// Import angular-fusioncharts
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+// Import FusionCharts library and chart modules
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    FusionChartsModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
